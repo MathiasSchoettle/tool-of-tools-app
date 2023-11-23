33b05633b2443238ca6ace9fa53bf2fb7850ed3c
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-11-23 20:38:39 +0100

    removed query test code

--- src/App.js
+++ src/App.js
@@ -6,14 +6,10 @@ import Pomodoro from './pomodoro/Pomodoro'
 import Sidebar from './Sidebar'
 import Calendar from './newcalendar/Calendar'
 import {default as Old} from './calendar/Calendar'
-import Test from "./Test";
-import SuccessButton from "./inputs/button/SuccessButton";
 
 export default function App() {
     let [current, set_current] = useState(5)
     let [sidebar_opened, set_sidebar_opened] = useState(false)
-
-    const [show, setShow] = useState(false);
 
     function get_content() {
         switch (current) {
@@ -26,13 +22,7 @@ export default function App() {
             case 4:
                 return <Pomodoro />
             default:
-                return (
-                    <div className="h-full w-full flex flex-col gap-2">
-                        <SuccessButton content="Toggle" onClick={() => setShow(!show)}/>
-                        <Test name={"first"}/>
-                        {show && <Test name={"second"}/>}
-                    </div>
-                )
+                return <></>
         }
     }
 
