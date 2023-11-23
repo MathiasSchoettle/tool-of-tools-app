9a02236427e81cf84617557a70a299c972e1d900
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-11-23 03:11:02 +0100

    reworded queryAge

--- src/query/QueryHook.js
+++ src/query/QueryHook.js
@@ -2,10 +2,10 @@ import {useCallback, useContext, useState, useSyncExternalStore} from "react";
 import {QueryContext} from "./QueryProvider";
 import QueryObserver from "./QueryObserver";
 
-export default function useQuery(queryKey, queryFn, staleTime) {
+export default function useQuery(queryKey, queryFn, queryAge) {
 
 	const queryClient = useContext(QueryContext);
-	const [observer] = useState(() => new QueryObserver(queryKey, queryFn, staleTime, queryClient));
+	const [observer] = useState(() => new QueryObserver(queryKey, queryFn, queryAge, queryClient));
 
 	const subscribe = useCallback((listener) => {
 		return observer.register(listener);
