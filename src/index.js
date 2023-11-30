546ed7c8e3f82e20df19d5b1e6c454322a828772
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-11-30 22:47:28 +0100

    removed react query tests

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App";
 import QueryProvider from "./query/QueryProvider";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const queryClient = new QueryClient()
-
 root.render(
-
-	<QueryClientProvider client={queryClient}>
-		<QueryProvider>
-			<App/>
-		</QueryProvider>
-	</QueryClientProvider>
-
+	<QueryProvider>
+		<App/>
+	</QueryProvider>
 );
 
